25404be3ea6d1433f54ce2234f747ef9778d63e6
Mariia M <blucktea@gmail.com>
2022-03-16 17:11:09 +0200

    addded keyboard

--- src/index.ts
+++ src/index.ts
@@ -25,8 +25,48 @@ const arrQuestionToAnswer = [
     {
         'question': 'Что ты умеешь?',
         'answer': 'Очень мало чего. Я молодой бот.'
+    },
+    {
+        'question': 'Опрос',
+        'answer': 'Кто из них?',
+        'option': {
+            reply_markup: {
+                inline_keyboard: [
+                    [
+                        {
+                            'text': 'Жабки',
+                            'callback_data': '1'
+                        }
+                    ],
+                    [
+                        {
+                            'text': 'Котики',
+                            'callback_data': '2'
+                        }
+                    ],
+                    [
+                        {
+                            'text': 'Лисята',
+                            'callback_data': '3'
+                        }
+                    ]
+                ]
+            }
+        }
     }
 ]
+
+bot.on('callback_query', (query) => {
+    if (query.data === '1') {
+        bot.sendMessage(query.message.chat.id, 'Ты лягушонок')
+    }
+    if (query.data === '2') {
+        bot.sendMessage(query.message.chat.id, 'Ты котёнок')
+    }
+    if (query.data === '3') {
+        bot.sendMessage(query.message.chat.id, 'Ты лисёнок')
+    }
+} )
 
 bot.on('message', (msg) => {
     const chatId = msg.chat.id;
@@ -34,7 +74,7 @@ bot.on('message', (msg) => {
 
     const questionAndAnswer = arrQuestionToAnswer.find((el) => el['question'] === msg.text);
     if (questionAndAnswer) {
-        bot.sendMessage(chatId, questionAndAnswer['answer'])
+        bot.sendMessage(chatId, questionAndAnswer['answer'], questionAndAnswer.option)
     } else {
         // bot.sendMessage(chatId, 'Извини, я молодой бот. Могу чего-то не знать.')
         axios.get('https://api.thecatapi.com/v1/images/search').then(result => {
